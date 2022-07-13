b721a8e3242eff70ee2eba23f69d0d02398b1f30
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-07-13 20:02:52 +0100

    Update Graph Demo Algorithm

--- Graph_Algorithms/graph_demo.py
+++ Graph_Algorithms/graph_demo.py
@@ -1,14 +1,37 @@
+from termcolor import colored
+
 class Graph():
     def __init__(self, graph_dictionary: dict = None):
         if graph_dictionary is None:
             graph_dictionary = {}
         self.graph_dictionary = graph_dictionary
 
-    def add_edge(self, vertex: str, edge: str):
-        self.graph_dictionary[vertex].append(edge)
+    def __str__(self)-> str:
+        return str(self.graph_dictionary)
 
-graph = Graph()
-try:
+    def add_edge(self, vertex: str, edge: str):
+        try:
+            self.graph_dictionary[vertex].append(edge)
+        except KeyError:
+            self.graph_dictionary[vertex] = [edge]
+
+if __name__ == '__main__':
+    print(colored('---------------------- GRAPH ----------------------', 'red'))
+    graph = Graph()
+
+    print(colored('---------------------- EDGE TO AN EMPTY GRAPH ----------------------', 'red'))
     graph.add_edge('A', 'B')
-except KeyError:
-    print('Vertice A does not exist already')
+    graph.add_edge('A', 'C')
+    print(graph)
+
+    print(colored('---------------------- EDGE TO AN EXISTING GRAPH ----------------------', 'red'))
+    custom_dict = { "a": ["b", "c"],
+                    "b": ["a", "d", "e"],
+                    "c": ["a", "e"],
+                    "d": ["b", "e", "f"],
+                    "e": ["d", "f"],
+                    "f": ["d", "e"]
+    }
+    new_graph = Graph(custom_dict)
+    new_graph.add_edge("e", "c")
+    print(new_graph)
