a692d1d62e1a29e3adf12f86870eb8d54f873a80
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-07-31 10:35:34 +0100

    refator

--- Graph_Algorithms/all_sources_shortest_path.py
+++ Graph_Algorithms/all_sources_shortest_path.py
@@ -16,8 +16,8 @@ def floyd_warshall(no_vertices: int, graph: List[list]):
         for i in range(no_vertices): # 2D iteration
             for j in range(no_vertices):
                 distance[i][j] = min(distance[i][j], 
-                                                distance[i][vertice] + distance[vertice][j]
-                                                )
+                                     distance[i][vertice] + distance[vertice][j]
+                                    )
         # print_solution(no_vertices, distance)
         # print('----------')
     # print('Solution')
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         [4, inf, 0, inf],
         [inf, 2, 9, 0],
     ]
-    print(colored('------------ FLOYD WARSHALL ALGORIGHTM ------------', 'red'))
+    print(colored('------------ FLOYD WARSHALL ALGORITHM ------------', 'red'))
     print(colored('------------------- GIVEN GRAPH -------------------', 'red'))
     print_solution(len(graph), graph)
     print(colored('--------------------- SOLUTION --------------------', 'red'))
