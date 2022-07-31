1c287eb78aba070c366d438c80ff572161f7255c
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-07-31 19:41:05 +0100

    minimum spanning tree - Add Disjoint Set

--- Graph_Algorithms/minimum_spanning_tree.py
+++ Graph_Algorithms/minimum_spanning_tree.py
@@ -1,11 +1,37 @@
 from termcolor import colored
+from typing import List
 
 class DisjointSet():
-    def __init__(self, vertices, parent):
+    def __init__(self, vertices: List[str]):
         self.vertices = vertices
-        self.parent = parent
+        self.parent = {}
         self.rank = dict.fromkeys(vertices, 0)
+        for v in vertices:
+            self.parent[v] = v
+
+    def find(self, item: str):
+        if self.parent[item] == item:
+            return item
+        else:
+            return self.find(self.parent[item])
+
+    def union(self, x: str, y: str):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        if self.rank[xroot] < self.rank[yroot]:
+            self.parent[xroot] = yroot
+        elif self.rank[xroot] > self.rank[yroot]:
+            self.parent[yroot] = xroot
+        else:
+            self.parent[yroot] = xroot
+            self.rank[xroot] += 1
 
 if __name__ == '__main__':
     print(colored('----------------- GRAPH ALGORITHMS ----------------', 'red'))
+    print(colored('-------------- MINIMUM SPANNING TREE --------------', 'red'))
+    vertices = ['A', 'B', 'C', 'D', 'E']
+    disjoint_set = DisjointSet(vertices)
+    disjoint_set.union('A', 'B')
+    disjoint_set.union('A', 'C')
+    print(disjoint_set.find('A'))
     
