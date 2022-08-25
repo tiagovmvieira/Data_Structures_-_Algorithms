0032ca9dee1c84eeb0ea3fb26a75700761b74517
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2022-08-25 22:43:32 +0100

    Finish Route Between Nodes quizz

--- Graph_Algorithms/route_between_nodes.py
+++ Graph_Algorithms/route_between_nodes.py
@@ -1,4 +1,3 @@
-from distutils import dep_util
 from termcolor import colored
 
 class Graph():
@@ -10,31 +9,43 @@ class Graph():
     def __str__(self)-> str:
         return str(self.graph_dictionary)
 
+    def _arrange_graph(self):
+        visited_vertices = []
+        visited_keys = []
+        for key in self.graph_dictionary:
+            visited_keys.append(key)
+            visited_vertices.append(key) if key not in visited_vertices else None
+            for value in self.graph_dictionary.get(key):
+                visited_vertices.append(value) if value not in visited_vertices else None
+    
+        a = set(visited_keys).union(set(visited_vertices))
+        b = set(visited_keys).intersection(set(visited_vertices))
+        c = list(a - b)
+
+        auxiliar_graph = dict.fromkeys(c, [])
+        self.graph_dictionary.update(auxiliar_graph)
+
     def add_edge(self, vertex: str, edge: str):
         try:
             self.graph_dictionary[vertex].append(edge)
         except KeyError:
             self.graph_dictionary[vertex] = [edge]        
-        
+
     def check_route(self, start_node: str, end_node: str)-> bool:
+        self._arrange_graph()
         visited_nodes = [start_node]
         queue = [start_node]
         path = False
         while queue:
             dequeue_vertex = queue.pop(0) #dequeue first element
-            try:
-                for adjacent_vertex in self.graph_dictionary.get(dequeue_vertex):
-                    print(adjacent_vertex)
-                    if adjacent_vertex not in visited_nodes:
-                        if adjacent_vertex == end_node:
-                            path = True
-                            break
-                        else:
-                            visited_nodes.append(adjacent_vertex)
-                            queue.append(adjacent_vertex)
-            except KeyError:
-                self.graph_dictionary[adjacent_vertex] = []
-                visited_nodes.append(adjacent_vertex)
+            for adjacent_vertex in self.graph_dictionary.get(dequeue_vertex):
+                if adjacent_vertex not in visited_nodes:
+                    if adjacent_vertex == end_node:
+                        path = True
+                        break
+                    else:
+                        visited_nodes.append(adjacent_vertex)
+                        queue.append(adjacent_vertex)
         return path
 
 if __name__ == '__main__':
